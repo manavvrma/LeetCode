630cd8c382cdc20bc82e3002abbf31bd999033b8
Manav Verma <manavvrma17@gmail.com>
2024-06-12 11:47:38 +0530

    Create Problem-04.java

---
+++ Problem-04.java
@@ -0,0 +1,25 @@
+// 49. Group Anagrams
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map <String,List<String>> map = new HashMap<>();
+
+        for(String s:strs){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+
+
+            }
+
+            map.get(sorted).add(s);
+        }
+        return new ArrayList<>(map.values());
+        
+    }
+}
